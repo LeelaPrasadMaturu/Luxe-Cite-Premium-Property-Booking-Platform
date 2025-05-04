212eed4a3f81055d607b92df99009a79875cfd07
Surya Sujit <35captainmarvel@gmail.com>
2025-05-04 21:23:23 +0530

    added CI/CD

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,19 @@
-export default {
+module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
+  verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
-  verbose: true,
-  setupFilesAfterEnv: ['./__tests__/setup.js'],
-  transform: {},
-  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node']
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.js'
+  ],
+  setupFiles: ['<rootDir>/__tests__/setup.js']
 }; 
