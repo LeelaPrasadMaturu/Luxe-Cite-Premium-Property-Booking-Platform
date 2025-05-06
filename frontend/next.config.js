0c692cf07f53c146e7f1a7b2bc68d2c9435df434
Surya Sujit <35captainmarvel@gmail.com>
2025-05-06 12:31:16 +0530

    added vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
-    domains: ['localhost', 'images.unsplash.com'],
-  },
-  experimental: {
-    serverActions: true,
+    domains: [
+      'images.unsplash.com',
+      'images.pexels.com',
+      'localhost'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
